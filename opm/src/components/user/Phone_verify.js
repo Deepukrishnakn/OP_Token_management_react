1d9344f6f99a071828759bfb7c9a80cd1915cbe2
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-29 13:19:48 +0530

    depantancy

---
+++ opm/src/components/user/Phone_verify.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+
+function Phone_verify() {
+  return (
+    <div>
+        
+
+        <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+
+       
+     
+      {/* <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="With sx" variant="standard" />
+      </Box> */}
+    </Box>
+
+
+    </div>
+  )
+}
+
+export default Phone_verify
---
+++ opm/src/pages/PhoneVerifyPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Phone_verify from '../components/user/Phone_verify'
+
+function PhoneVerifyPage() {
+  return (
+    <div>
+        
+
+<Phone_verify/>
+
+
+    </div>
+  )
+}
+
+export default PhoneVerifyPage
